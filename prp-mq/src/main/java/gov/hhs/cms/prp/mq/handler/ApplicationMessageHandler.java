d7a172e3f8c6a3a86d2eda18cb0aa20d3542eef6
Joe Arsenault <jarsenx@gmail.com>
2015-12-04 10:18:37 -0500

    Refactor createObjectsFromString method to superclass.

--- prp-mq/src/main/java/gov/hhs/cms/prp/mq/handler/ApplicationMessageHandler.java
+++ prp-mq/src/main/java/gov/hhs/cms/prp/mq/handler/ApplicationMessageHandler.java
@@ -1,18 +1,9 @@
 package gov.hhs.cms.prp.mq.handler;
 
-import com.blackbear.flatworm.ConfigurationReader;
-import com.blackbear.flatworm.FileFormat;
-import com.blackbear.flatworm.MatchedRecord;
 import gov.hhs.cms.prp.dao.bean.PrpAplctnDAOBean;
 import gov.hhs.cms.prp.entity.PrpAplctnEntity;
 import gov.hhs.cms.prp.entity.PrpPlanoptionsEntity;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -44,42 +35,15 @@ public class ApplicationMessageHandler extends MQMessageHandler {
             if (messageBody.length() > 70) {
                 messageBody = insertNewlineBetweenSegments(messageBody, 70);
             }
-            entityList = createObjectFromString(configFilePath, messageBody, "planOption", "planOption");
+            entityList = createObjectsFromString(configFilePath, messageBody, "planOption", "planOption");
             savePlanoptionEntities(entityList);
         } else {
-            entityList = createObjectFromString(configFilePath, messageBody, "application", "application");
+            entityList = createObjectsFromString(configFilePath, messageBody, "application", "application");
             saveAplctnEntity(entityList);
         }
 
         LOGGER.log(Level.INFO, entityList.toString());
 
-
-    }
-
-    public List<Object> createObjectFromString(String configFilePath, String messageString, String recordName, String beanName) throws Exception {
-        LOGGER.log(Level.INFO, "Entering createObjectFromString");
-        Object entity = null;
-        List<Object> entityList = new ArrayList<Object>();
-
-        ConfigurationReader parser = new ConfigurationReader();
-
-        // TODO Load the configuration file at startup and store the FileFormat object in a Map with the ones for the other message types.
-        FileFormat ff = parser.loadConfigurationFile(configFilePath);
-        InputStream stream = new ByteArrayInputStream(messageString.getBytes(StandardCharsets.UTF_8));
-        BufferedReader bufIn = new BufferedReader(new InputStreamReader(stream));
-        MatchedRecord results;
-        while ((results = ff.getNextRecord(bufIn)) != null) {
-            LOGGER.log(Level.INFO, results.toString());
-            if (results.getRecordName().equals(recordName)) {
-                entity = (Object)results.getBean(beanName);
-                LOGGER.log(Level.INFO, entity.toString());
-                entityList.add(entity);
-            }
-        }
-
-        LOGGER.log(Level.INFO, entityList.toString());
-        LOGGER.log(Level.INFO, "Exiting createObjectFromString");
-        return entityList;
 
     }
 
--- prp-mq/src/main/java/gov/hhs/cms/prp/mq/handler/MQMessageHandler.java
+++ prp-mq/src/main/java/gov/hhs/cms/prp/mq/handler/MQMessageHandler.java
@@ -1,10 +1,27 @@
 package gov.hhs.cms.prp.mq.handler;
 
+import com.blackbear.flatworm.ConfigurationReader;
+import com.blackbear.flatworm.FileFormat;
+import com.blackbear.flatworm.MatchedRecord;
 import gov.hhs.coms.prp.dao.factory.MySQLDAOFactory;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Created by jarsen on 11/5/15.
  */
 public abstract class MQMessageHandler {
+
+    private final static Logger LOGGER = Logger.getLogger(MQMessageHandler.class.getName());
+
 
     protected static MySQLDAOFactory mySQLDAOFactory;
 
@@ -14,8 +31,6 @@ public abstract class MQMessageHandler {
         }
         return mySQLDAOFactory;
     }
-
-    public abstract Object createObjectFromString(String configFilePath, String messageString, String recordName, String beanName) throws Exception;
 
     public static String insertNewlineBetweenSegments(String messageBody, int segmentLength) {
         int nbrOfSegments = messageBody.length() / segmentLength;
@@ -30,4 +45,32 @@ public abstract class MQMessageHandler {
 
         return builder.toString();
     }
+
+    public static List<Object> createObjectsFromString(String configFilePath, String messageString, String recordName, String beanName) throws Exception {
+        LOGGER.log(Level.INFO, "Entering createObjectsFromString");
+        Object entity = null;
+        List<Object> entityList = new ArrayList<Object>();
+
+        ConfigurationReader parser = new ConfigurationReader();
+
+        // TODO Load the configuration file at startup and store the FileFormat object in a Map with the ones for the other message types.
+        FileFormat ff = parser.loadConfigurationFile(configFilePath);
+        InputStream stream = new ByteArrayInputStream(messageString.getBytes(StandardCharsets.UTF_8));
+        BufferedReader bufIn = new BufferedReader(new InputStreamReader(stream));
+        MatchedRecord results;
+        while ((results = ff.getNextRecord(bufIn)) != null) {
+            LOGGER.log(Level.INFO, results.toString());
+            if (results.getRecordName().equals(recordName)) {
+                entity = (Object)results.getBean(beanName);
+                LOGGER.log(Level.INFO, entity.toString());
+                entityList.add(entity);
+            }
+        }
+
+        LOGGER.log(Level.INFO, entityList.toString());
+        LOGGER.log(Level.INFO, "Exiting createObjectsFromString");
+        return entityList;
+
+    }
+
 }
